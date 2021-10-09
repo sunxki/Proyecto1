ccd39844dd6fbf5015ded01a707ab78e87576977
sunxki <dgallegosm1200@alumno.ipn.mx>
2021-10-09 13:39:53 -0700

    First commit

---
+++ Proyecto1/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template,Context
+from django.template.loader import get_template
+from django.shortcuts import render
+
+class Champ:
+
+    def __init__(self,name, rol):
+        self.name = name
+        self.rol = rol
+
+def saludo(request):
+    Vayne = Champ("Vayne" , "ADC")
+
+    return render(request,"home.html",{
+        "characterName": Vayne.name,
+        "characterRol": Vayne.rol
+    })
+
+def calculadora(request,a,b):
+    division = a/b
+    _time = datetime.datetime.now()
+
+    return render(request,"calculator.html", {
+        "First_value": a,
+        "Second_value": b,
+        "division": round(division,4),
+        "time":_time.now()
+    })
+
+
+
+
